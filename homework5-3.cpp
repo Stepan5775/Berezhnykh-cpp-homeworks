8bceb1485322cb8543cade91602c92ebf9f1800e
Stepan5775 <stepanberezhnykh@gmail.com>
2025-11-03 14:36:54 +0500

    Create homework5-3.cpp

---
+++ homework5-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int sum = 0;
+    cout << "Введите N: ";
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        sum += i; 
+    }
+    cout << "Сумма: " << sum << endl;
+    return 0;
+}
