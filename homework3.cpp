07f0a83e4d448cd77ee0b5275f54455ca521bd93
Stepan5775 <stepanberezhnykh@gmail.com>
2025-11-03 14:22:19 +0500

    Update homework3.cpp

--- homework3.cpp
+++ homework3.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, b;
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Остаток от деления: " << a % b << endl;
+    return 0;
+}
