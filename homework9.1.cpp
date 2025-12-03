636add1d189c60273f0d3dab1c90ab3f484937ed
Berezhnykh_ <stepanberezhnykh@gmail.com>
2025-12-03 19:29:07 +0500

    Create homework9.1.cpp

---
+++ homework9.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sumOfThree(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+    int a, b, c;
+    cout << "Введите три числа: ";
+    cin >> a >> b >> c;
+
+    int result = sumOfThree(a, b, c);
+
+    cout << "Сумма: " << result << endl;
+
+    return 0;
+}
