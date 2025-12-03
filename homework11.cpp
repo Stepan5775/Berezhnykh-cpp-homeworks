169d083aee9ddfe3d3598597aa32f3dbc9bb79b2
Berezhnykh_ <stepanberezhnykh@gmail.com>
2025-12-03 19:40:05 +0500

    Update homework11.cpp

--- homework11.cpp
+++ homework11.cpp
@@ -14,32 +14,24 @@ int value(char c) {
     }
     return 0;
 }
-
 int romanToInt(const string& s) {
     int total = 0;
-
     for (int i = 0; i < s.length(); i++) {
         int curr = value(s[i]);
         int next = (i + 1 < s.length()) ? value(s[i + 1]) : 0;
-
         if (next > curr) {
             total -= curr;
         } else {
             total += curr;
         }
     }
-
     return total;
 }
-
 int main() {
     string s;
     cout << "Введите римское число: ";
     cin >> s;
-
     int result = romanToInt(s);
-
     cout << "Десятичное значение: " << result << endl;
-
     return 0;
 }
