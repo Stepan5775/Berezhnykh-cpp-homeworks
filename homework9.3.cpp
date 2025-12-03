4ceb18d3beab6b1ec93b6c271c95af9a7bd112cf
Berezhnykh_ <stepanberezhnykh@gmail.com>
2025-12-03 19:32:28 +0500

    Create homework9.3.cpp

---
+++ homework9.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumToN(int n) {
+    if (n == 1) return 1;
+    return n + sumToN(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Введите N: ";
+    cin >> n;
+
+    int result = sumToN(n);
+
+    cout << "Сумма: " << result << endl;
+
+    return 0;
+}
