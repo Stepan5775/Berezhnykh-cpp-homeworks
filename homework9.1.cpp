694b3de25e55e3417f45b64efeac2cdaa69cdb35
Berezhnykh_ <stepanberezhnykh@gmail.com>
2025-12-03 19:38:24 +0500

    Update homework9.1.cpp

--- homework9.1.cpp
+++ homework9.1.cpp
@@ -4,15 +4,11 @@ using namespace std;
 int sumOfThree(int a, int b, int c) {
     return a + b + c;
 }
-
 int main() {
     int a, b, c;
     cout << "Введите три числа: ";
     cin >> a >> b >> c;
-
     int result = sumOfThree(a, b, c);
-
     cout << "Сумма: " << result << endl;
-
     return 0;
 }
