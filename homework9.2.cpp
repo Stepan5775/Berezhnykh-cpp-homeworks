b8cc8de07a91c1d68edb292cdcf09573dbb13339
Berezhnykh_ <stepanberezhnykh@gmail.com>
2025-12-03 19:38:48 +0500

    Update homework9.2.cpp

--- homework9.2.cpp
+++ homework9.2.cpp
@@ -6,24 +6,19 @@ void doubleArray(int arr[], int size) {
         arr[i] *= 2;
     }
 }
-
 int main() {
     const int SIZE = 5;
     int arr[SIZE] = {1, 2, 3, 4, 5};
-
     cout << "Исходный массив: ";
     for (int i = 0; i < SIZE; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
-
     doubleArray(arr, SIZE);
-
     cout << "После удвоения: ";
     for (int i = 0; i < SIZE; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
-
     return 0;
 }
