b2d17629ead0baa30fcb6a17ef32c30f6b194a86
Berezhnykh_ <stepanberezhnykh@gmail.com>
2025-12-03 19:39:09 +0500

    Update homework9.3.cpp

--- homework9.3.cpp
+++ homework9.3.cpp
@@ -5,15 +5,11 @@ int sumToN(int n) {
     if (n == 1) return 1;
     return n + sumToN(n - 1);
 }
-
 int main() {
     int n;
     cout << "Введите N: ";
     cin >> n;
-
     int result = sumToN(n);
-
     cout << "Сумма: " << result << endl;
-
     return 0;
 }
